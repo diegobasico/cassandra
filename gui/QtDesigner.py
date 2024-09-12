6da95b6add3b429ceba278e8f7d6a90f58abac61
Diego Alonso <diego.ugaz13@gmail.com>
2024-09-12 00:50:16 -0500

    fixed QtDesigner

--- gui/QtDesigner.py
+++ gui/QtDesigner.py
@@ -1,10 +1,38 @@
-from os import startfile
-from platform import node
+# import os
+# import subprocess
+# from platform import node
+
+# qtdesigner_path = {
+#     'windows10': r"C:\Users\Diego\miniconda3\envs\cassandra\Library\lib\qt6\bin\designer.exe",
+#     'archlinux': r"/home/diego/miniconda3/envs/cassandra/lib/qt6/bin/designer"
+# }
+
+# path = {
+#     'windows10': r"os.startfile(C:\Users\Diego\miniconda3\envs\cassandra\Library\lib\qt6\bin\designer.exe)",
+#     'archlinux': r"subprocess.run(/home/diego/miniconda3/envs/cassandra/lib/qt6/bin/designer)"
+# }
+
+# system_hostname = node()
+# exec(path[system_hostname])
+
+import os
+import subprocess
+import platform
+
 
 qtdesigner_path = {
-    'windows10': r"C:\Users\Diego\miniconda3\envs\cassandra\Library\lib\qt6\bin\designer.exe",
-    'archlinux': r""
+    'Windows': r"C:\Users\Diego\miniconda3\envs\cassandra\Library\lib\qt6\bin\designer.exe",
+    'Linux': r"/home/diego/miniconda3/envs/cassandra/lib/qt6/bin/designer"
 }
+system = platform.system()
 
-system_hostname = node()
-startfile(qtdesigner_path[system_hostname])
+def main():
+    if system == 'Windows':
+        os.startfile(qtdesigner_path['Windows'])  # For Windows
+    elif system == 'Linux':
+        subprocess.run([qtdesigner_path['Linux']])  # For Linux
+    else:
+        print(f"Operating system '{system}' is not supported.")
+
+if __name__ == '__main__':
+    main()
